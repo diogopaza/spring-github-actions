81b1665ff51d024cdccbea9590725e4ee1943a06
Diogo Paza <paza@educabiz.com>
2025-12-24 08:09:36 -0300

    protegendo a main

--- src/main/java/com/exercicio_spring_github_actions/ExercicioSpringGithubActionsApplication.java
+++ src/main/java/com/exercicio_spring_github_actions/ExercicioSpringGithubActionsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExercicioSpringGithubActionsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("protegendo a main");
 		SpringApplication.run(ExercicioSpringGithubActionsApplication.class, args);
 	}
 
